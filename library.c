04b12d453cfdb78048eb29f66b35291c5eaaa6c5
mertKelkit <mertkelkit@marun.edu.tr>
2017-05-12 15:46:02 +0300

    Update library.c

--- library.c
+++ library.c
@@ -7,6 +7,9 @@
 #define AUTHOR_MODE 2
 #define UPDATE_BOOK_NAME 1
 #define UPDATE_AUTHOR 2
+#define MAIN_MENU 0
+#define UPDATE_MENU 1
+#define FAILURE -1
 
 int selectedMode = ID_MODE;
 
@@ -26,19 +29,24 @@ typedef struct n
 Node* root = NULL;
 
 void menu(void);
-Node* readFromTextFile(void);
 void print(void);
-void createInfo(void);
-Node* addToList(Book, int);
-void deleteInfo(void);
-Node* deleteFromList(int);
 void updateInfo(void);
+void createInfo(void);
+void deleteInfo(void);
+void writeToTextFile(void);
 int updateCheck(int);
-Node* update(int, int);
+int myPow(int, int);
+int charControlID(char*);
+int charControlAddID(char*);
+int charControlChoice(char*, int);
+char* charControlAuthor(char*);
 Node* sortForID(void);
 Node* sortForBookName(void);
 Node* sortForAuthor(void);
-void writeToTextFile(void);
+Node* readFromTextFile(void);
+Node* deleteFromList(int);
+Node* update(int, int);
+Node* addToList(Book, int);
 
 int main()
 {
@@ -53,10 +61,15 @@ void menu()
 	while(1)
     {
         int choice;
+        char choiceC[20];
         printf("\nLIBRARY PROGRAM\n----------------\n1 - List all books\n2 - Update a book\n3 - Create a new book\n4 - Delete a book\n");
         printf("5 - Sort all books according to id\n6 - Sort all books according to book name\n");
         printf("7 - Sort all books according to author name\n8 - Exit\nEnter your choice: ");
-        scanf("%d", &choice);
+        do
+        {
+        	scanf("%s", choiceC);
+        } while(charControlChoice(choiceC, MAIN_MENU) == FAILURE);
+        choice = charControlChoice(choiceC, MAIN_MENU);
         switch (choice) {
             case 1:
             	print();
@@ -75,21 +88,31 @@ void menu()
             	if(root == NULL)
             		printf("\nThere is no book but if you want to add a book, book will be added ID-sorted.\n");
                 else
+                {
                 	root = sortForID();
+                	print();
+                }
+
                 break;
             case 6:
             	selectedMode = NAME_MODE;
             	if(root == NULL)
             		printf("\nThere is no book but if you want to add a book, book will be added book name-sorted.\n");
                 else
+                {
                 	sortForBookName();
+                	print();
+                }
                 break;
             case 7:
             	selectedMode = AUTHOR_MODE;
             	if(root == NULL)
             		printf("\nThere is no book but if you want to add a book, book will be added author name-sorted.\n");
                 else
+                {
                 	sortForAuthor();
+                	print();
+                }
                 break;
             case 8:
             	if(root != NULL)
@@ -139,7 +162,6 @@ Node* readFromTextFile()
 			iter -> next = NULL;
 			//deleting last node because there is a problem with reading the text file...
 			prev -> next = iter -> next;
-			free(iter);
 		}
 		fclose(stream);
 	}
@@ -163,9 +185,9 @@ void print()
 	while(iter != NULL)
 	{
 		if(iter == root)
-			printf("\n%d %s %s\n", iter -> book.id, iter -> book.bookName, iter -> book.author);
+			printf("\nID -> %d\tBook Name -> %s\tAuthor Name -> %s\n", iter -> book.id, iter -> book.bookName, iter -> book.author);
 		else
-			printf("%d %s %s\n", iter -> book.id, iter -> book.bookName, iter -> book.author);
+			printf("ID -> %d\tBook Name -> %s\tAuthor Name -> %s\n", iter -> book.id, iter -> book.bookName, iter -> book.author);
 		iter = iter -> next;
 	}
 }
@@ -173,13 +195,22 @@ void print()
 void createInfo()
 {
 	Book book;
+	char tempID[10], tempAuthor[30];
     printf("\nPlease enter the book ID: ");
-    scanf("%d", &book.id);
+    do
+    {
+    	scanf("%s", tempID);
+    } while(charControlAddID(tempID) == FAILURE);
+    book.id = charControlAddID(tempID);
     printf("Please enter the book name: ");
     scanf("%s", book.bookName);
     printf("Please enter the author's name: ");
-    scanf("%s", book.author);
+    do
+    {
+    	scanf("%s", book.author);
+    } while(charControlAuthor(book.author) == NULL);
     root = addToList(book, selectedMode);
+    printf("\nAdded successfuly !\nID -> %d\tBook Name -> %s\tAuthor Name -> %s\n", book.id, book.bookName, book.author);
 }
 
 Node* addToList(Book b, int mode)
@@ -269,10 +300,15 @@ Node* addToList(Book b, int mode)
 void deleteInfo()
 {
 	int id;
+	char idC[10];
 	if(root != NULL)
 	{
 		printf("\nEnter an id for the book which you want to delete: ");
-		scanf("%d", &id);
+		do
+		{
+			scanf("%s", idC);
+		} while(charControlID(idC) == FAILURE);
+		id = charControlID(idC);
 		root = deleteFromList(id);
 	}
 	else
@@ -288,11 +324,13 @@ Node* deleteFromList(int id)
     {
         if(iter -> book.id == id)
         {
+        	printf("\nID -> %d\tBook Name -> %s\tAuthor Name -> %s\n", iter -> book.id, iter -> book.bookName, iter -> book.author);
             if(prev == NULL)
             {
                 root = iter -> next;
                 iter -> next = NULL;
                 free(iter);
+                printf("\nDeleted successfuly !\n");
                 return root;
             }
             else
@@ -300,6 +338,7 @@ Node* deleteFromList(int id)
                 prev -> next = iter -> next;
                 iter -> next = NULL;
                 free(iter);
+                printf("\nDeleted successfuly !\n");
                 return root;
             }
         }
@@ -316,16 +355,31 @@ Node* deleteFromList(int id)
 
 void updateInfo()
 {
-	int id;
-    printf("\nEnter an ID for the book which you want to update: ");
-    scanf("%d", &id);
-    if(updateCheck(id))
-    {   
-        int choice;
-        printf("\n1 - Update book name\n2 - Update author name\nEnter you choice: ");
-        scanf("%d", &choice);
-        root = update(id, choice);
-    }
+	if(root != NULL)
+	{
+		int id;
+		char idC[10];
+	    printf("\nEnter an ID for the book which you want to update: ");
+	    do
+	    {
+	    	scanf("%s", idC);
+	    } while(charControlID(idC) == FAILURE);
+	    id = charControlID(idC);
+	    if(updateCheck(id))
+	    {   
+	        int choice;
+	        char choiceC[10];
+	        printf("\n1 - Update book name\n2 - Update author name\nEnter you choice: ");
+	        do
+	        {
+	        	scanf("%s", choiceC);
+	        } while(charControlChoice(choiceC, UPDATE_MENU) == FAILURE);
+	        choice = charControlChoice(choiceC, UPDATE_MENU);
+	        root = update(id, choice);
+		}    
+	}
+	else
+		printf("\nThere is no book :(\n");
 }
 
 int updateCheck(int id)
@@ -335,13 +389,12 @@ int updateCheck(int id)
     {
         if(iter -> book.id == id)
         {
-        	free(iter);
+        	printf("\nID -> %d\tBook Name -> %s\tAuthor Name -> %s\n", iter -> book.id, iter -> book.bookName, iter -> book.author);
             return 1;
         }
         iter = iter -> next;
     }
     printf("\nCannot found ID %d in the library!\n", id);
-    free(iter);
     return 0;
 }
 
@@ -359,7 +412,13 @@ Node* update(int id, int choice)
 		}
 		printf("\nEnter new book name: ");
 		scanf("%s", newBookName);
-		strcpy(iter -> book.bookName, newBookName);
+		Book b;
+		b.id = iter -> book.id;
+		strcpy(b.bookName, newBookName);
+		strcpy(b.author, iter -> book.author);
+		root = deleteFromList(iter -> book.id);
+		root = addToList(b, selectedMode);
+
 	}
 	else if(choice == UPDATE_AUTHOR)
 	{
@@ -371,10 +430,19 @@ Node* update(int id, int choice)
 			iter = iter -> next;
 		}
 		printf("\nEnter new author name: ");
-		scanf("%s", newAuthor);
-		strcpy(iter -> book.author, newAuthor);
+		do
+		{
+			scanf("%s", newAuthor);
+		} while(charControlAuthor(newAuthor) == NULL);
+		Book b;
+		b.id = iter -> book.id;
+		strcpy(b.bookName, iter -> book.bookName);
+		strcpy(b.author, newAuthor);
+		root = deleteFromList(iter -> book.id);
+		root = addToList(b, selectedMode);
 	}
-	free(iter);
+	printf("\nUpdated successfuly !\n");
+	printf("\nID -> %d\tBook Name -> %s\tAuthor Name -> %s\n", iter -> book.id, iter -> book.bookName, iter -> book.author);
 	return root;
 }
 
@@ -456,4 +524,102 @@ void writeToTextFile()
 			root = root -> next;
 		}
 	fclose(stream);
+}
+
+int charControlID(char* input)
+{
+	int i, val=0;
+	int ctr = 0;
+	for(i=strlen(input)-1; i>=0; i--)
+	{
+		if(input[i] > 57 || input[i] < 49)
+		{
+			printf("\nID must be a positive integer. Please enter again: ");
+			return FAILURE;
+		}
+		val += (input[i]-48)*myPow(10, ctr);
+		ctr++;
+	}
+	return val;
+}
+
+int charControlChoice(char* input, int mode)
+{
+	if(mode == UPDATE_MENU)
+	{
+		int i, val = 0, ctr = 0;
+		for(i = strlen(input)-1; i>=0; i--)
+		{
+			if(input[i] < 49 || input[i] > 50)
+			{
+				printf("\nChoice must be an integer between 1 and 2. Please enter again: ");
+				return FAILURE;
+			}
+			val += (input[i]-48)*myPow(10, ctr);
+			return val;
+		}
+	}
+	else if(mode == MAIN_MENU)
+	{
+		int i, val = 0, ctr = 0;
+		for(i = strlen(input)-1; i>=0; i--)
+		{
+			if(input[i] < 49 || input[i] > 56)
+			{
+				printf("\nChoice must be an integer between 1 and 8. Please enter again: ");
+				return FAILURE;
+			}
+			val += (input[i]-48)*myPow(10, ctr);
+			return val;
+		}
+	}
+}
+
+char* charControlAuthor(char* input)
+{
+	int i;
+	for(i=strlen(input)-1; i>=0; i--)
+	{
+		if(input[i] <= 57 && input[i] >= 48)
+		{
+			printf("\nAuthor name cannot contain an integer. Please enter again: ");
+			return NULL;
+		}
+	}
+	return input;
+}
+
+int charControlAddID(char* input)
+{
+	int i, ctr = 0, val = 0;
+	Node* iter = root;
+	for(i = strlen(input)-1; i>=0; i--)
+	{
+		if(input[i] > 57 || input[i] < 48)
+		{
+			printf("\nID must be a positive integer. Please enter again: ");
+			return FAILURE;
+		}
+		val += (input[i]-48)*myPow(10, ctr);
+		ctr++;
+	}
+	while(iter != NULL)
+	{
+		if(iter -> book.id == val)
+		{
+			printf("\nThis ID exists. Please enter again: ");
+			return FAILURE;
+		}
+		iter = iter -> next;
+	}
+	return val;
+}
+
+int myPow(int base, int e)
+{
+	int i;
+	int ret = 1;
+	for(i=0; i<e; i++)
+		ret *= base;
+	return ret;
 }
